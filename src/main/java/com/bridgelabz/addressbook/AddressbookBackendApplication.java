4d31e07e1cc4401c832c23d2517b6cbcc7c6a996
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-09 15:04:59 +0530

    Added the welcome message

--- src/main/java/com/bridgelabz/addressbook/AddressbookBackendApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressbookBackendApplication {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook");
         SpringApplication.run(AddressbookBackendApplication.class, args);
     }
 
