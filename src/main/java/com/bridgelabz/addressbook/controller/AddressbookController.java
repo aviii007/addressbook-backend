0a1cc619fe0d33031be0cea41a948cf15264e260
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-09 15:41:58 +0530

    Added the dto in the addressbook app

--- src/main/java/com/bridgelabz/addressbook/controller/AddressbookController.java
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressbookController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook.controller;
 
+import com.bridgelabz.addressbook.dto.AddressbookDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
